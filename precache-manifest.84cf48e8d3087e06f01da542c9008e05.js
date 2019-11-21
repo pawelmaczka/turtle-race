22abc33471268c7162a0e370fd0e2ac72982fa83
pawelmaczka <pawel@maczka.it>
2019-11-21 17:59:18 +0100

    Updates

---
+++ precache-manifest.84cf48e8d3087e06f01da542c9008e05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9399b36dd11f1207339c99e7f8b780f0",
+    "url": "/turtle-race/index.html"
+  },
+  {
+    "revision": "d5f746adc0f9b62d1d23",
+    "url": "/turtle-race/static/js/2.fa01a2ad.chunk.js"
+  },
+  {
+    "revision": "9da0e52bd527c5e50ec5",
+    "url": "/turtle-race/static/js/main.37c594be.chunk.js"
+  },
+  {
+    "revision": "b6f248d341bfc1f21e1d",
+    "url": "/turtle-race/static/js/runtime-main.455ca9e8.js"
+  },
+  {
+    "revision": "423911c4f3d3951cee34515f4602baa2",
+    "url": "/turtle-race/static/media/turtle.423911c4.svg"
+  }
+]);
